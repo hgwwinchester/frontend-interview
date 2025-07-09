7e9ec622173293c7be291a803e3192c53faeb8d6
Henry Winchester <hgwwinchester@gmail.com>
2025-07-09 14:48:57 +0100

    Load more button loads next page

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -1,20 +1,38 @@
-import React from "react";
-import SingleApplication from "./SingleApplication";
-import { getSingleApplicationFixture } from "./__fixtures__/applications.fixture";
+import React, {FC, useCallback, useState} from "react";
 import styles from "./Applications.module.css";
 import {Button} from "./ui/Button/Button.tsx";
+import useApplicationsStore from "./useApplicationsStore.tsx";
+import SingleApplication from "./SingleApplication.tsx";
+import {TApplication} from "./types/TApplication.ts";
+
+type ApplicationsListProps = {
+  applications: TApplication[]
+}
+
+const ApplicationsList: FC<ApplicationsListProps> = ({applications}) => {
+  return <ol className={styles.ApplicationsList}>
+    {applications.map((application) => (
+      <li key={application.id}>
+        <SingleApplication application={application}/>
+      </li>
+    ))}
+  </ol>
+}
+
+const ApplicationsLoading: FC = () => {
+  return <p className={styles.Loading}>Loading applications...</p>
+}
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
+  const [page, setPage] = useState(0);
+  const {data: applications} = useApplicationsStore(page, 5)
+
+  const loadMoreFn = useCallback(() => setPage(page => page + 1), [setPage])
 
   return (
     <div className={styles.Applications}>
-      <ol className={styles.ApplicationsList}>
-        <li>
-          <SingleApplication application={applications[0]} />
-        </li>
-      </ol>
-      <Button className={styles.LoadMore}>
+      {applications ? <ApplicationsList applications={applications}/> : <ApplicationsLoading/>}
+      <Button className={styles.LoadMore} onClick={loadMoreFn}>
         Load more
       </Button>
     </div>
--- src/useApplicationsStore.tsx
+++ src/useApplicationsStore.tsx
@@ -6,7 +6,7 @@ const fetcher = (key: string) => fetch(key).then(res => res.json())
 const useApplicationsStore = (page: number, limit: number, suspense = false): SWRResponse<TApplication[]> => {
   if (page < 0) throw new Error('Page must be greater than 0')
   if (limit < 0) throw new Error('Limit must be greater than 0')
-  const key = `?_page=${page}&_limit=${limit}`
+  const key = `http://localhost:3001/api/applications?_page=${page}&_limit=${limit}`
   return useSWR(key, fetcher, {suspense})
 }
 
