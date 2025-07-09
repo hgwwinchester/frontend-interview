0d17783e8bd58d2d57965c28552ad9588cae12e2
Henry Winchester <hgwwinchester@gmail.com>
2025-07-09 15:19:14 +0100

    Added MSW setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,5 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import {getSingleApplicationFixture} from "./__fixtures__/applications.fixture.js"
 
 /// <reference types="vitest" />
+
+import {http, HttpResponse} from "msw";
+import {setupServer} from "msw/node";
+
+const applications = Array.from({length: 50}, () => getSingleApplicationFixture[0])
+
+const restHandlers = [
+  http.get('http://localhost:3001/api/applications', () => {
+    return HttpResponse.json(applications);
+  })
+]
+
+const server = setupServer(...restHandlers);
+
+beforeAll(async () => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
